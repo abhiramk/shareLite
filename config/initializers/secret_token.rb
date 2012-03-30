40b29842f716647ee79b2a530aa2c5469c130d0b
Abhiram Kaushik <abhiramk@flipkart.com>
2012-03-31 03:41:57 +0530

    initial setup

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ShareLite::Application.config.secret_token = '74b039a72ab472f1826746c4ed04b6e918d150cefa3f3f498f1dab0eec9074c21c1a5f172c36a0cd3b7a737cb4916306424c4f6778168bb2f193344b9d48c026'
